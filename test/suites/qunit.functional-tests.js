b774f9d0dcb0956cf1b14bbcd52ce63c6bb2a5c0
Tim Huegdon <tim@nefariousdesigns.co.uk>
2012-07-12 06:46:42 +0100

    Rearrange tests in preparation for more init unit tests.

--- test/suites/qunit.functional-tests.js
+++ test/suites/qunit.functional-tests.js
@@ -9,23 +9,6 @@
             $slider = $(sliderClass).slider();
         }
     });
-    test('Validate markup', function() {
-        // Check all the markup is in the right place
-        deepEqual($(".slider").length, 1,
-            "Slider wrapper exists.");
-        deepEqual($(".slider-container").length, 1,
-            "Slider container exists.");
-        deepEqual($(sliderClass + " + .slider-container").length, 1,
-            "Slider container is directly after the original text box.");
-        deepEqual($(".slider-handle").length, 1,
-            "Slider handle exists.");
-        deepEqual($(".slider-strip").length, 1,
-            "Slider strip exists.");
-        deepEqual($(".slider-fill").length, 1,
-            "Slider fill exists.");
-        deepEqual($(".slider-strip .slider-fill").length, 1,
-            "Slider fill is within the slider strip.");
-    });
     test('Drag slider handle', function() {
         var sliderHandleLeft, expectedHandleLeft,
             sliderFillWidth, expectedFillWidth,
--- test/suites/qunit.unit-tests.js
+++ test/suites/qunit.unit-tests.js
@@ -3,12 +3,42 @@
     var $slider,
         sliderClass = '.video-seek';
 
+    //--------------------------------
+
+    module('Unit tests on init function');
+
+    //--------------------------------
+
+    test('Validate markup', function() {
+        $slider = $(sliderClass).slider();
+        // Check all the markup is in the right place
+        deepEqual($(".slider").length, 1,
+            "Slider wrapper exists.");
+        deepEqual($(".slider-container").length, 1,
+            "Slider container exists.");
+        deepEqual($(sliderClass + " + .slider-container").length, 1,
+            "Slider container is directly after the original text box.");
+        deepEqual($(".slider-handle").length, 1,
+            "Slider handle exists.");
+        deepEqual($(".slider-strip").length, 1,
+            "Slider strip exists.");
+        deepEqual($(".slider-fill").length, 1,
+            "Slider fill exists.");
+        deepEqual($(".slider-strip .slider-fill").length, 1,
+            "Slider fill is within the slider strip.");
+    });
+
+    //--------------------------------
+
     module('Unit tests on exposed functions', {
         setup: function() {
             // Setup default slider for the following tests
             $slider = $(sliderClass).slider();
         }
     });
+
+    //--------------------------------
+
     test('getLimitObject success', function() {
         var $sliderContainer = $('.slider-container'),
             $sliderHandle = $('.slider-handle'),
